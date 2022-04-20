da476d594b0f095fb50d0eb7a61c441fed54e596
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2022-04-20 08:51:05 -0300

    :construction: Add professional service Dto & ViewModel

---
+++ AppointmentService.Shared/Dto/ProfessionalServiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AppointmentService.Shared.Dto
+{
+    public sealed class ProfessionalServiceDto
+    {
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
---
+++ AppointmentService.Shared/ViewModels/ProfessionalServiceViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AppointmentService.Shared.ViewModels
+{
+    public sealed class ProfessionalServiceViewModel
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
