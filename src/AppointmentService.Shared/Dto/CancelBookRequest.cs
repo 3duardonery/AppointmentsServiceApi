4dd5f64562f51aca17784efc1851fae34d48f277
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2022-06-03 13:28:16 -0300

    :hammer: Change method of controller
    Change DELETE to POST

--- src/AppointmentService.Shared/Dto/CancelBookRequest.cs
+++ src/AppointmentService.Shared/Dto/CancelBookRequest.cs
@@ -1,11 +1,8 @@
-﻿using System;
-
-namespace AppointmentService.Shared.Dto
+﻿namespace AppointmentService.Shared.Dto
 {
     public sealed class CancelBookRequest
     {
         public string BookId { get; set; }
-        public DateTime CancelDate { get; set; }
         public string CancelBy { get; set; }
         public string Reason { get; set; }
     }
