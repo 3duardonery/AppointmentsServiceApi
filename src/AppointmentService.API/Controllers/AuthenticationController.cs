5979dbe338f8b639851b2745c34c261ceb774059
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2023-03-05 18:11:45 -0300

    feat: Add sentry performance to login

--- src/AppointmentService.API/Controllers/AuthenticationController.cs
+++ src/AppointmentService.API/Controllers/AuthenticationController.cs
@@ -2,6 +2,7 @@
 using AppointmentService.Shared.Dto;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Sentry;
 using System.Threading.Tasks;
 
 namespace AppointmentService.API.Controllers
@@ -12,20 +13,30 @@ namespace AppointmentService.API.Controllers
     public class AuthenticationController : ControllerBase
     {
         private readonly AuthenticationServiceImp _authenticationService;
+        private readonly IHub _sentryHub;
 
-        public AuthenticationController(AuthenticationServiceImp authenticationService) 
-            => _authenticationService = authenticationService;
+        public AuthenticationController(AuthenticationServiceImp authenticationService, IHub sentryHub)
+        {
+            _authenticationService = authenticationService;
+            _sentryHub = sentryHub;
+        }
 
         [HttpPost]
         public async Task<IActionResult> LogIn([FromBody] AuthenticationRequestDto request)
         {
+            var childSpan = _sentryHub.GetSpan()?.StartChild("authentication-firebase");
             if (!ModelState.IsValid)
                 return BadRequest();
 
             var (isSuccess, result, exception) = await _authenticationService.LogIn(request).ConfigureAwait(false);
 
             if (!isSuccess)
+            {
+                childSpan.Finish(exception);
                 return Unauthorized();
+            }
+
+            childSpan.Finish(SpanStatus.Ok);
 
             return Ok(result);
         }
